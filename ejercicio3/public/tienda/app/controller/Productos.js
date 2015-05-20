cabb28b3c200c2ea72b511436330321bd78e10a3
Rodolfo Campos <camposer@gmail.com>
2015-05-20 11:46:50 +0200

    Solución del ejercicio3 (parte2 - 2)

--- ejercicio3/public/tienda/app/controller/Productos.js
+++ ejercicio3/public/tienda/app/controller/Productos.js
@@ -15,7 +15,13 @@ Ext.define('Tienda.controller.Productos', {
             },
             'edicionproducto button[action=guardar]': {
                 click: this.guardarProducto
-            }            
+            },    
+            'listaproductos button[action=eliminar]': {
+                click: this.eliminarProducto
+            },
+            'listaproductos button[action=agregar]': {
+                click: this.agregarProducto
+            }
         });        
     },
 
@@ -25,16 +31,43 @@ Ext.define('Tienda.controller.Productos', {
         view.down('form').loadRecord(record);
     },
 
+    agregarProducto: function(grid, record) {
+        var view = Ext.widget('edicionproducto');
+        view.setTitle('Agregar producto');
+        //view.title = 'Agregar producto'; // no funciona porque es un config!
+    },
+
 	guardarProducto: function(button) {
 	    var win    = button.up('window'),
 	        form   = win.down('form'),
 	        record = form.getRecord(), 
 	        values = form.getValues(); 
 
-	    record.set(values);
+        if (record) { // modificando
+            record.set(values);
+        } else { // agregando
+            record = Ext.create('Tienda.model.Producto', {
+                nombre: values.nombre,
+                precio: values.precio
+            });
+
+            //record.get('nombre')
+            //this.getProductosStore().add(values);
+            this.getProductosStore().add(record);
+        }
+
 	    win.close();
 
 	    // synchronize the store after editing the record
 	    this.getProductosStore().sync();
-	}    
+        this.getProductosStore().load();
+	},
+
+    eliminarProducto: function(button) {
+        var grid    = button.up('listaproductos'),
+            records = grid.getSelectionModel().getSelection();
+
+        this.getProductosStore().remove(records);
+        this.getProductosStore().sync();
+    }
 });
--- ejercicio3/public/tienda/app/view/producto/Lista.js
+++ ejercicio3/public/tienda/app/view/producto/Lista.js
@@ -4,12 +4,40 @@ Ext.define('Tienda.view.producto.Lista' ,{
 
     store: 'Productos',
     title: 'Productos',
+    multiSelect: true,
 
     initComponent: function() {
         this.columns = [
             {header: 'Id',  dataIndex: 'id',  flex: 1},
             {header: 'Nombre',  dataIndex: 'nombre',  flex: 1},
-            {header: 'Precio', dataIndex: 'precio', flex: 1}
+            {header: 'Precio', dataIndex: 'precio', flex: 1},
+            {
+                xtype:'actioncolumn',
+                width:50,
+                items: [{
+                    icon: '/ext-4/examples/restful/images/delete.png',
+                    tooltip: 'Eliminar',
+                    handler: function(grid, rowIndex, colIndex) {
+                        var rec = grid.getStore().getAt(rowIndex);
+                        
+                        grid.getStore().remove(rec);
+                        grid.getStore().sync();
+                    }
+                }]
+            }
+        ];
+
+        this.tbar = [
+            '->',
+            {
+                text: 'Agregar',
+                action: 'agregar'
+            },
+            '-',
+            {
+                text: 'Eliminar',
+                action: 'eliminar'
+            }
         ];
 
         this.callParent(arguments);
